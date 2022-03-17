79466280a91a2ab0e4d2185e084569a840f97d80
adminov <batishnazik@gmail.com>
2022-03-17 21:49:51 +0600

    'settingProject-NavBar'

---
+++ src/Components/NavBar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import logoImg from "../image/logo.svg";
+
+
+const NavBarStyled = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    height: 80px;
+    width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    background-color: #299B01;
+    color: white;  
+`;
+
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const H1 = styled.h1`
+    font-size: 24px;
+   margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+    width: 50px;
+`;
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid black;
+  border-radius: 3px;
+`;
+
+export const NavBar = () => (
+    <NavBarStyled>
+        <Logo>
+            <ImgLogo src={logoImg} alt="logo"/>
+            <H1>MrDonald's</H1>
+        </Logo>
+        <Button>Войти</Button>
+    </NavBarStyled>
+);
